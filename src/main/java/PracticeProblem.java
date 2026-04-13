7d5ae6d0a1e5f3a0b0d992b2eaf3ee79a8fcceeb
weiyaliu <343445839@tdsb.ca>
2026-04-13 20:41:06 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,45 @@
+/**
+	* Lesson: Nested Ifs
+	* Author: Weiya
+	* Date Created: March 23, 2023
+	* Date Last Modified: March 26, 2026
+	*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+
+	//1.
+	public static int min(int a, int b, int c) {
+		if (a < b) {
+			if (a < c) {
+				return a;
+			}
+		} else {
+			if (b < c) {
+				return b;
+			}
+		
+		} return c;
+	}
+
+	//2. 
+	public static boolean isLeapYear (int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return true;
+			}
+		} else { 
+				return false;
+	}
+
+}
 
 }
